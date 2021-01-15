84270984b5d7cf2f528c915bea1c86e284350371
Rodrigo Barros <rodrigobarros110@hotmail.com>
2021-01-15 15:57:24 -0300

    Update index.js

--- aula15/index.js
+++ aula15/index.js
@@ -1,8 +1,19 @@
 console.log(!!(100 / 0));
 
 // let num2 = Math.floor(num1);
+/* floor arredonda pra baixo */
+
 // let num2 = Math.ceil(num1);
+/* ceil arredonda pra cima */
+
 // let num2 = Math.round(num1);
+/* pra arredondar de forma automática */
+
 // console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6));
+/*  max pegar o maior número */
+
 // console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6));
+/* min pra pegar o menor número */
+
 // const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+/* pra gerar um numero aleatório entre 0 e 1, so que o 1 nunca é incluido */
