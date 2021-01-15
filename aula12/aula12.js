e97208a09b280f52284da9feffcb6783be7da871
Rodrigo Barros <rodrigobarros110@hotmail.com>
2021-01-15 15:57:29 -0300

    Update aula12.js

--- aula12/aula12.js
+++ aula12/aula12.js
@@ -1,7 +1,22 @@
-let varA = 'A'; // B
-let varB = 'B'; // C
-let varC = 'C'; // A
+let varA = 'A' // B 
+let varB = 'B' // C 
+let varC = 'C' // A
 
-[varA, varB, varC] = [varB, varC, varA];
+const varATemp = varA
+
+varA = varB
+varB = varC
+varC = varATemp
 
 console.log(varA, varB, varC);
+
+/* resolvendo de outra maneira */
+
+let varA = 'A' // B 
+let varB = 'B' // C 
+let varC = 'C' // A
+
+[varA, varB, varC] = [varB, VarC, varA]
+
+console.log(varA, varB, varC);
+
