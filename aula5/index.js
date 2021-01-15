5866ef6f91d2af54a602beb0878b424e3746b20d
Rodrigo Barros <rodrigobarros110@hotmail.com>
2021-01-15 15:57:09 -0300

    Create index.js

---
+++ aula5/index.js
@@ -0,0 +1,29 @@
+const num1 = 5
+const num2 = 2
+const num3 = 10
+
+console.log(num1 ** num2);
+/* (potenciação) numero 1 elevado a numero 2 */
+
+console.log(num1 % num2);
+/* resto da divisão */
+
+console.log((num1 + num2) * num3);
+/* 70*/
+
+console.log(num1 + num2 / num3);
+/* 5.2 */
+
+console.log(++num1);
+/* pré encremento */
+
+console.log(num1++);
+
+console.log(num1--);
+/* decremento */
+
+let contador = 0 
+contador += 2; /* é a mesma coisa que contador = contador + 2 */ 
+
+contador *= 2; /* é a mesma coisa que contador = contador * 2 */ 
+
