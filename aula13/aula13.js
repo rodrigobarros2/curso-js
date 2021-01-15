1b1e47a72bccd7bf2cc4787cfe302c386a557250
Rodrigo Barros <rodrigobarros110@hotmail.com>
2021-01-15 15:57:27 -0300

    Update aula13.js

--- aula13/aula13.js
+++ aula13/aula13.js
@@ -1,4 +1,4 @@
-//               012345678
+//   012345678
 let umaString = 'Meu valor';
 
 // length
@@ -12,9 +12,17 @@ console.log(umaString.lastIndexOf('e', umaString.length)); // Retorna o índice
 console.log(umaString.match(/[A-Za-z]+/g)); // Retorna um array com os valores encontrados (se g)
 console.log(umaString.search(/[a-z]+/g)); // Retorna o índice da primeira ocorrência
 console.log(umaString.replace(/e/g, '3'));
+
 console.log(umaString.slice(2, 7));
-console.log(umaString.slice(-3, -1));
+/*  pega a string da posição 2 até a posição 7 */
+
+console.log(umaString.slice(-5, -1));
+/* vai pegar as 5 ultimas palavras do texto menos a ultima */
+
 console.log(umaString.split(' ', 2));
+/* cada palavra(obs, não letra) do texto vai ser dividida por um espaço */
+
 console.log(umaString.toUpperCase());
+/* deixa a string em maiusculo */
 console.log(umaString.toLowerCase());
 
