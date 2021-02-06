2b058b3c842034ca323eeeeb53444da4bf7c4bc0
Rodrigo Barros <rodrigobarros110@hotmail.com>
2021-02-06 01:08:19 -0300

    Update index.js

--- aula34/index.js
+++ aula34/index.js
@@ -1,3 +1,10 @@
 const frutas = ['Maçã', 'Pêra', 'Uva'];
 const novasFrutas = frutas.map(el => el.toLowerCase());
 console.log(novasFrutas); // [ 'maçã', 'pêra', 'uva' ]
+
+
+const charDota = ['Medusa', 'Bara', 'Abadon'];
+
+for (let i = 0; i < charDota.length; i++){
+    console.log(`índice ${i} personagem`, charDota[i]);
+}
